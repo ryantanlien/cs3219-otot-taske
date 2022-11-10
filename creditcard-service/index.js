5d984f66fe7416377259cfffed342a065319d7fb
Ryan Tan <ryantanlien99@gmail.com>
2022-11-10 23:59:44 +0800

    Rename folder

--- creditcard-service/index.js
+++ creditcard-service/index.js
@@ -6,7 +6,6 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { readCreditCardsQuery, insertCreditCardsQuery, deleteCreditCardsQuery } from './creditcards/database.js'; 
 import { cacheCreditCards, clearCreditCardsCache, getCreditCardsFromCache } from './creditcards/redis.js';
-import { readdirSync } from 'fs';
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename);
